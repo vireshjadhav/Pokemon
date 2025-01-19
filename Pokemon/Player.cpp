9b228b962f9da734a4e1c07427bcfe447069f3b5
Viresh_Jadhav <168194192+vireshjadhav@users.noreply.github.com>
2025-01-20 00:35:36 +0530

    Implement Player class functions and members in player.cpp
    Created player.cpp to define the Player class functions and members. Implemented the logic for the choosePokemon function. Added definitions for constructors and other member functions. Ensured proper initialization of class variables and functionality.

---
+++ Pokemon/Player.cpp
@@ -0,0 +1,48 @@
+//Player.cpp
+include "Player.hpp"
+include "iostream"
+using namespace std;
+
+
+    //Default constructor
+    Player::Player() {
+        name = "Trainer";
+        chosenPokemon = Pokemon();//Using the default Pokemon constructor
+    }
+  
+    //Parameterized constructor
+    Player::Player(string p_name, Pokemon p_chosenPokemon) {
+        name = p_name;
+        chosenPokemon = p_chosenPokemon;
+    }
+
+    //Copy constructor
+    Player::Player(const Player& other)
+    {
+        name = other.name;
+        chosenPokemon = other.chosenPokemon;
+    }
+
+    //Function to choose a pokemon
+    void Player::choosePokemon(int choice)
+    {
+        //Store the choice integer to respective pokemon
+        switch ((PokemonChoice)choice)
+        {
+        case PokemonChoice::CHARMANDER:
+            chosenPokemon = Pokemon("Charmander", PokemonType::FIRE, 100);
+            break;
+        case PokemonChoice::BULBASAUR:
+            chosenPokemon = Pokemon("Bulbasaur", PokemonType::GRASS, 100);
+            break;
+        case PokemonChoice::SQUIRTLE:
+            chosenPokemon = Pokemon("Squirtle", PokemonType::WATER, 100);
+            break;
+        default:
+            chosenPokemon = Pokemon("Pikachu", PokemonType::ELECTRIC, 100);
+        }
+        cout << "Player " << name << " chose " << chosenPokemon.name << "!" << endl;
+
+        Utility::waitForEnter();
+    }
+};
