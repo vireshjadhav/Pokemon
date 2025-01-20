7fe3047894eef7de3793e6d1897d896d75588955
Viresh_Jadhav <168194192+vireshjadhav@users.noreply.github.com>
2025-01-20 12:54:52 +0530

    Implement Pokemon class functions and members in Pokemon.cpp
    Created Pokemon.cpp to define the Player class functions and members. Implemented the logic for the attack function. Added definitions for constructors and other member functions. Ensured proper initialization of class variables and functionality.

---
+++ Pokemon/Pokemon.cpp
@@ -0,0 +1,44 @@
+#include "Pokemon.hpp"
+#include "PokemonChoice.hpp"
+#incldue "PokemonType.hpp"
+#include "Player.hpp"
+#include "Utility.hpp
+#incldue <iostream>
+using namespace std;
+
+//Default constructor
+Pokemon::Pokemon()
+{
+  name = "Unknown";
+  type = PokemonType::NORMAL;
+  health = 50;
+}
+
+//Parameterized constructor
+Pokemon::Pokemon(string p_name, PokemonType p_type, int p_health)
+{
+  name = p_name;
+  type = p_type;
+  health = p_health;
+}
+
+//Copy constructor
+Pokemon::Pokemon(const Pokemon& other)
+{
+  name = other.name;
+  type = other.type;
+  health = other.health;
+}
+
+
+//Destructor
+Pokemon::~Pokemon()
+{
+}
+
+
+// Function to demonstrate attack
+void Pokemon::attack() 
+{
+  cout << name << "attacks with a powerful move!" << endl;
+}
